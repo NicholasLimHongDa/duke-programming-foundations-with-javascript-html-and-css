6a034f03da8bd122a4e832a12b10c66019209409
NicholasLimHongDa <nicholas.lim.hong.da.cs@gmail.com>
2023-07-10 22:30:00 +0900

    2: Added some material to Module 3

---
+++ Module 3/green-screen-web-page/script.js
@@ -0,0 +1,18 @@
+function upload() {
+    var imgcanvas = document.getElementById("canvas1");
+    var fileinput = document.getElementById("finput");
+    // Declare "image" as a global variable
+    image = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas);
+}
+
+function makeGray() {
+    for (var pixel of image.values()){
+        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgcanvas = document.getElementById("canvas1");
+    image.drawTo(imgcanvas);
+}
