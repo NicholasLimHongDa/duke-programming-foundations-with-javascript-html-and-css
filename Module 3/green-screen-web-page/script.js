004f0a7c0dc2e26fa058a859ee8e94590b6644be
NicholasLimHongDa <lim@aisl.cs.tut.ac.jp>
2023-08-25 00:30:38 +0900

    3: Added green-screen-webpage.html

--- Module 3/green-screen-web-page/script.js
+++ Module 3/green-screen-web-page/script.js
@@ -16,3 +16,24 @@ function makeGray() {
     var imgcanvas = document.getElementById("canvas1");
     image.drawTo(imgcanvas);
 }
+
+function uploadDouble() {
+    var imgcanvas1 = document.getElementById("canvas1");
+    var imgcanvas2 = document.getElementById("canvas2");
+    var fileinput = document.getElementById("finput");
+    image = new SimpleImage(fileinput);
+    imageGray = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas1);
+    imageGray.drawTo(imgcanvas2);
+}
+
+function makeGrayDouble() {
+    for (var pixel of imageGray.values()){
+        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var imgcanvas = document.getElementById("canvas2");
+    imageGray.drawTo(imgcanvas);
+}
